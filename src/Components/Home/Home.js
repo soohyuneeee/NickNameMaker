e27cb61461b8e9d8ac5667bd3acdfbed7085f193
soohyuneeee <soohyuneeee12@gmail.com>
2022-11-25 10:12:10 +0900

    Add - 배경 바뀌면서 글자바뀌는 홈

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,10 +5,48 @@ function Home() {
     let navigate = useNavigate();
     return (
         <section>
-            <div className='haha'>
-                <button class="nickname-btn" onClick={() => {
-                    navigate('/maker')
-                }}>NICKNAME MAKER</button>
+            <div class='masthead-image' id='master-container'>
+                <div class='content center'>
+                    <h1 id='master'>
+                        <div>Hello</div>
+                        <div id='master-container-scroller'>
+                            <div class='master-container-scroller_item'>
+                                BSSM.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Teacher.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Friend.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Junior.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Senior.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Gamer.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Principal.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Originator.
+                            </div>
+                            <div class='master-container-scroller_item'>
+                                Couple.
+                            </div>
+                            <div class='master-container-scroller_item'>Everyone.</div>
+                        </div>
+                        <div>Nice to see you.</div>
+                    </h1>
+                    <div className='haha'>
+                        <button class="nickname-btn" onClick={() => {
+                            navigate('/login')
+                        }}>Let's GENERATE</button>
+                    </div>
+                </div>
             </div>
         </section>
 
