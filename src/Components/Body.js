43a3b73a38c1276aaefccacd2fa7699752d4a05c
soohyuneeee <soohyuneeee12@gmail.com>
2022-11-25 13:57:21 +0900

    Style - 코드  정렬

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,9 @@
 import React from "react";
-function Body(){
-    return(
-    <div className="duv">
-    <button className="b1">NICKNAME MAKER</button>
-    </div>
+function Body() {
+    return (
+        <div className="duv">
+            <button className="b1">NICKNAME MAKER</button>
+        </div>
     )
 }
 export default Body
