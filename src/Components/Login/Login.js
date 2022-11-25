ad06ed78ef526a372e93fa26f777ca3e5be02cca
soohyuneeee <soohyuneeee12@gmail.com>
2022-11-25 14:00:09 +0900

    Add - 로그인 제목

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,31 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Navigate, useNavigate } from 'react-router-dom';
 import styled from "styled-components"
 
 
-function Login(){
-    return(
+function Login() {
+    return (
         <Contain>
-        <Dogin>
-            <img src={"../../../public/android-icon-96x96.png"}/>
-            <Text><b><span>아이디를 입력해 주세요.</span></b>
-                <input type="text" style={{height:"10%" ,width:"100%",paddingLeft:"10px",borderRadius:"10px"}}></input>
-                <b><span>비밀번호를 입력해 주세요.</span></b>
-                <input type="password"style={{height:"10%",width:"100%",paddingLeft:"10px",borderRadius:"10px"}}></input>
-            </Text>
-            <Button>
-            <Button1>로그인하기</Button1>
-            <Button2>회원가입 하기</Button2>
-            </Button>
-            <Without>로그인 하지 않고 시작하기</Without>
-        </Dogin></Contain>
+            <Dogin>
+                <h2 style={{ fontSize: "30px", marginTop: "50px" }}><b>Login</b></h2>
+                <Text><b><span>아이디를 입력해 주세요.</span></b>
+                    <input type="text" style={{ height: "10%", width: "100%", paddingLeft: "10px", borderRadius: "10px" }}></input>
+                    <b><span>비밀번호를 입력해 주세요.</span></b>
+                    <input type="password" style={{ height: "10%", width: "100%", paddingLeft: "10px", borderRadius: "10px" }}></input>
+                </Text>
+                <Button>
+                    <Button1>로그인하기</Button1>
+                    <Button2>회원가입 하기</Button2>
+                </Button>
+                <Without><a href="/maker" style={{ textDecoration: "none", color: "gray" }}>로그인 하지 않고 시작하기</a></Without>
+            </Dogin></Contain>
     )
 }
 export default Login;
+
 const Contain = styled.div`
-    margin-top: 5%;
     width: 100%;
-    height: 100%;
+    height: 87vh;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -39,6 +39,10 @@ const Dogin = styled.div`
     width:60%;
     height:70ex;
     border-radius:10px;
+    transition: 0.25s;
+    &:hover{
+        transform:scale(1.01)
+    }
     
 `;
 const Button = styled.div`
